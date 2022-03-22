de54cf380ccd53de63b00274dcad56051198938d
yesarounBlog <akor11lyt@gmail.com>
2022-03-22 18:07:52 +0900

    first commit

---
+++ JDBC02_scott.sql
@@ -0,0 +1,46 @@
+SELECT USER
+  FROM DUAL;
+--==>> SCOTT
+
+TRUNCATE TABLE TBL_MEMBER;
+--==>> Table TBL_MEMBER이(가) 잘렸습니다.
+
+CREATE SEQUENCE MEMBERSEQ
+NOCACHE;
+--==>> Sequence MEMBERSEQ이(가) 생성되었습니다.
+
+
+--○ 데이터 입력 쿼리문 구성
+INSERT INTO TBL_MEMBER(SID, NAME, TEL)
+VALUES(MEMBERSEQ.NEXTVAL, '이호석', '010-1111-1111');
+
+
+INSERT INTO TBL_MEMBER(SID, NAME, TEL) VALUES(MEMBERSEQ.NEXTVAL, '이호석', '010-1111-1111')
+;
+
+--○ 인원수 확인 쿼리문 구성
+SELECT COUNT(*) AS COUNT
+  FROM TBL_MEMBER;
+--==>> 한줄 구성
+SELECT COUNT(*) AS COUNT   FROM TBL_MEMBER
+;
+--==>> 1
+
+
+--○ 전체 리스트 조회 쿼리문 구성
+SELECT SID, NAME, TEL
+  FROM TBL_MEMBER
+ORDER BY SID;
+--> 한줄 구성
+SELECT SID, NAME, TEL  FROM TBL_MEMBER ORDER BY SID
+;
+--==>> 1	이호석	010-1111-1111
+
+--○ 커밋
+COMMIT;
+--==>> 커밋 완료.
+
+
+
+
+
